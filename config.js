24b62bfb047084302e38663eb9f45847248a5adc
MohitsBranmac <ms24546@gmail.com>
2017-10-31 19:32:59 +0530

    Adds Config.js for firestore

---
+++ config.js
@@ -0,0 +1,10 @@
+const config = {
+  apiKey: "AIzaSyAxl42hgHmePaIGO22kLlZtdJbSkxrWK7M",
+  authDomain: "fir-chat-d2604.firebaseapp.com",
+  databaseURL: "https://fir-chat-d2604.firebaseio.com",
+  projectId: "fir-chat-d2604",
+  storageBucket: "fir-chat-d2604.appspot.com",
+  messagingSenderId: "843059856895"
+};
+
+export default config;
