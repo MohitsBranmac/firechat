fe1dedc4c9a5281f26bfeb9140176057326c2b29
MohitsBranmac <ms24546@gmail.com>
2017-10-31 21:32:03 +0530

    Adds Initialize component as child

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,19 @@
 <template>
   <div id="app">
-    
+    <Initialize />
   </div>
 </template>
 
 <script>
+import Initialize from './Initialize.vue';
+
 export default {
   name: 'app',
   created () {
     console.log ( this.$store.state.db );
+  },
+  components: {
+    Initialize
   }
 }
 </script>
--- store/index.js
+++ store/index.js
@@ -16,5 +16,8 @@ const state = {
 
 export default new Vuex.Store ({
   state,
-  modules: {}
+  modules: {
+    users,
+    conversations
+  }
 });
