e28d35c39015215677e2daea7989eec171fa9d1b
MohitsBranmac <ms24546@gmail.com>
2017-10-31 20:39:42 +0530

    Adds Users.js

---
+++ src/Initialize.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <button @click="init">Initialize App Base State</button>
+    <button @click="get">Get</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "InitializeData",
+
+    methods: {
+      init () {
+        this.$store.dispatch ('users/seed')
+        this.$store.dispatch ('conversations/seed')
+      },
+      get () {
+        //....
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
