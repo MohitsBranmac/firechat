dffd246a7e3918e1d0e7300be81e4d0ac07083eb
MohitsBranmac <ms24546@gmail.com>
2017-10-31 19:44:29 +0530

    Adds Store

---
+++ store/index.js
@@ -0,0 +1,20 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+import 'babel-polyfill';
+
+import Firebase from 'firebase';
+import 'Firebase/firestore';
+import config from '../config';
+
+firebase.initializeApp (config);
+
+Vue.use (Vuex);
+const state = {
+  db: Firebase.firestore ()    
+};
+
+export default new Vuex.Store ({
+  state,
+  modules: {}
+});
